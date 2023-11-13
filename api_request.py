35af4a51a8e8a4737b300a03b0d62776f7569d81
bolbas <cmrtumilovic@gmail.com>
2023-11-14 01:55:55 +0300

    update

--- api_request.py
+++ api_request.py
@@ -5,71 +5,40 @@ import datetime
 
 def date_regulate(n):
     today = datetime.date.today()
-    delta = n - today.weekday()
+    delta = n - today.weekday() if n is not None else 0
     result = (datetime.datetime.now() + datetime.timedelta(days=delta)).strftime("%d.%m")
     return result
 
 
 def get_subjects():
-    current_week = requests.get('https://iis.bsuir.by/api/v1/schedule/current-week')
-    res = requests.get('https://iis.bsuir.by/api/v1/schedule?studentGroup=373904')
-    data = json.loads(res.text)
-    week = json.loads(current_week.text)
-    wednesday = data["schedules"]["Среда"]
-    thursday = data["schedules"]["Четверг"]
-    friday = data["schedules"]["Пятница"]
-    saturday = data["schedules"]["Суббота"]
-    total_list = []
+    try:
+        current_week = requests.get('https://iis.bsuir.by/api/v1/schedule/current-week')
+        current_week.raise_for_status()
+        res = requests.get('https://iis.bsuir.by/api/v1/schedule?studentGroup=373904')
+        res.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data from API: {e}")
+        return []
 
-    for i in range(len(wednesday)):
-        if week in wednesday[i]["weekNumber"]:
-            if wednesday[i]["lessonTypeAbbrev"] == 'ЛР':
-                less = wednesday[i]["subject"]
-                if wednesday[i]["numSubgroup"] == 1:
-                    less += '(1)'
-                elif wednesday[i]["numSubgroup"] == 2:
-                    less += '(2)'
-                less += f' {date_regulate(2)}'
+    try:
+        data = json.loads(res.text)
+        week = json.loads(current_week.text)
 
-                total_list.append(less)
+        schedules = data.get("schedules", {})
+        total_list = []
 
-    for i in range(len(thursday)):
-        if week in thursday[i]["weekNumber"]:
-            if thursday[i]["lessonTypeAbbrev"] == 'ЛР':
-                less = thursday[i]["subject"]
-                if thursday[i]["numSubgroup"] == 1:
-                    less += '(1)'
-                elif thursday[i]["numSubgroup"] == 2:
-                    less += '(2)'
-                less += f' {date_regulate(3)}'
+        for day in schedules:
+            lessons = schedules[day]
+            for lesson in lessons:
+                if week in lesson.get("weekNumber", []):
+                    if lesson.get("lessonTypeAbbrev") == 'ЛР':
+                        less = lesson.get("subject", "")
+                        subgroup = f'({lesson.get("numSubgroup")})' if lesson.get("numSubgroup") else ""
+                        less += subgroup + f' {date_regulate(week)}'
+                        total_list.append(less)
 
-                total_list.append(less)
+        return total_list
+    except json.JSONDecodeError as e:
 
-    for i in range(len(friday)):
-        if week in friday[i]["weekNumber"]:
-            if friday[i]["lessonTypeAbbrev"] == 'ЛР':
-                less = friday[i]["subject"]
-                if friday[i]["numSubgroup"] == 1:
-                    less += '(1)'
-                elif friday[i]["numSubgroup"] == 2:
-                    less += '(2)'
-                less += f' {date_regulate(4)}'
-
-                total_list.append(less)
-
-    for i in range(len(saturday)):
-        if week in saturday[i]["weekNumber"]:
-            if saturday[i]["lessonTypeAbbrev"] == 'ЛР':
-                less = saturday[i]["subject"]
-                if saturday[i]["numSubgroup"] == 1:
-                    less += '(1)'
-                elif saturday[i]["numSubgroup"] == 2:
-                    less += '(2)'
-                less += f' {date_regulate(5)}'
-
-                total_list.append(less)
-
-    return total_list
-
-
-print(get_subjects())
+        print(f"Error parsing JSON data: {e}")
+        return []
