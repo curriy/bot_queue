a34d4c22121686a3e975f9ce815a7f3f21606593
Maksim Lis <mak.lis2016@gmail.com>
2023-11-16 23:26:50 +0300

    fix: fix schedule item day

--- api_request.py
+++ api_request.py
@@ -3,9 +3,19 @@ import json
 import datetime
 
 
+day_map = {
+    "Понедельник": 1,
+    "Вторник": 2,
+    "Среда": 3,
+    "Четверг": 4,
+    "Пятница": 5,
+    "Суббота": 6,
+    "Воскресенье": 7,
+}
+
 def date_regulate(n):
     today = datetime.date.today()
-    delta = n - today.weekday() if n is not None else 0
+    delta = n - today.weekday()
     result = (datetime.datetime.now() + datetime.timedelta(days=delta)).strftime("%d.%m")
     return result
 
@@ -34,7 +44,7 @@ def get_subjects():
                     if lesson.get("lessonTypeAbbrev") == 'ЛР':
                         less = lesson.get("subject", "")
                         subgroup = f'({lesson.get("numSubgroup")})' if lesson.get("numSubgroup") else ""
-                        less += subgroup + f' {date_regulate(week)}'
+                        less += subgroup + f' {date_regulate(day_map[day])}'
                         total_list.append(less)
 
         return total_list
